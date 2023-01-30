88f792599d89ebbd44f73f86a222eba6e7145b91
burakselcuk <burakselcuk1>
2023-01-30 10:30:25 +0300

    New Api retrofit instance created

--- app/src/main/java/com/example/contributors/common/utils/Constans.kt
+++ app/src/main/java/com/example/contributors/common/utils/Constans.kt
@@ -3,5 +3,7 @@ package com.example.contributors.common.utils
 class Constans {
     companion object {
         const val BASE_URL =" https://api.github.com/"
+        const val BASE_URLTWO ="https://bitpay.com/"
+
     }
 }
--- app/src/main/java/com/example/contributors/di/NetworkModule.kt
+++ app/src/main/java/com/example/contributors/di/NetworkModule.kt
@@ -1,7 +1,9 @@
 package com.example.contributors.di
 
 import com.example.contributors.common.utils.Constans.Companion.BASE_URL
+import com.example.contributors.common.utils.Constans.Companion.BASE_URLTWO
 import com.example.contributors.services.Api
+import com.example.contributors.services.ApiTwo
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
@@ -11,6 +13,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 
@@ -20,17 +23,39 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun getRetrofitServiceInstance(retrofit: Retrofit): Api {
+    @Named("first_retrofit")
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    @Named("second_retrofit")
+    fun provideRetrofitTwo(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URLTWO)
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun getRetrofitServiceInstance(@Named("first_retrofit") retrofit: Retrofit): Api {
         return retrofit.create(Api::class.java)
     }
 
+    @Singleton
+    @Provides
+    fun getRetrofitServiceInstanceTwo(@Named("second_retrofit") retrofit: Retrofit): ApiTwo {
+        return retrofit.create(ApiTwo::class.java)
+    }
 
     @Singleton
     @Provides
     fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
         .apply {
             level = HttpLoggingInterceptor.Level.BODY
-
         }
 
     @Singleton
@@ -41,18 +66,7 @@ object NetworkModule {
             .addInterceptor(httpLoggingIntercepter)
             .build()
 
-
-    @Singleton
-    @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(BASE_URL)
-        .client(okHttpClient)
-        .build()
-
-
     @Provides
     @Singleton
     fun provideGson() = Gson()
-
 }
